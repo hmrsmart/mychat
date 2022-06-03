31d4846b3630363b7f773ffac8db065cc5d67ff9
hmrsmart <97602593+hmrsmart@users.noreply.github.com>
2022-06-03 18:07:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+//const cookieParser = require("cookie-parser")
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+//app.use(cookieParser())
+// serve static assets normally
+app.use(express.static(path.join(__dirname, "public")))
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', (req, res) => {
+  //console.log(req.cookies);
+  //res.send("Success!")
+  res.sendFile(path.join(__dirname, "public", 'index.html'));
 });
 
-server.listen(8080, () => {
-  console.log('listening on *:8080');
-});
+app.listen(port);
+console.log("server started on port " + port);
